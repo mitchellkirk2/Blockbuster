6bee6a9a5fd9dc8e2b1918d7ed3db0cf9a7040d7
unknown <mitchellkirk2@yahoo.com>
2023-05-30 20:19:48 -0400

    added 2 tests

--- tests/book-dao.spec.ts
+++ tests/book-dao.spec.ts
@@ -12,3 +12,24 @@ test("Create a movie", async ()=> {
     console.log(result);
     expect(result.movieId).not.toBe(0);
 });
+
+// An integration test requires that two or more functoins you wrote pass
+
+test("Get movie by ID", async () =>{
+    let movie:Movie = new Movie(0, "Titanic", "James Cameron", true, 1);
+    movie = await movieDAO.createMovie(movie);
+
+    let retrievedMovie:Movie = await movieDAO.getMovieById(movie.movieId);
+    expect(retrievedMovie.title).toBe(movie.title);
+});
+
+test("Get all movies", async () =>{
+    let movie1:Movie = new Movie(0, "Avatar", "James Cameron", true, 0);
+    let movie2:Movie = new Movie(0, "The Wolf of Wall Street", "Martin Scorsese", true, 0);
+
+    await movieDAO.createMovie(movie1);
+    await movieDAO.createMovie(movie2);
+
+    const movies:Movie[] = await movieDAO.getAllMovies();
+    expect(movies.length).toBeGreaterThanOrEqual(2);
+});
--- tests/movie-dao-textfile-impl.ts
+++ tests/movie-dao-textfile-impl.ts
@@ -13,11 +13,21 @@ export class MovieDaoTextFile implements MovieDAO{
         await writeFile('C:\\Users\\Mitchell\\Desktop\\Blockbuster\\movies.txt', JSON.stringify(movies));
         return movie;
     }
-    getAllMovies(): Promise<Movie[]> {
-        throw new Error("Method not implemented.");
+    async getAllMovies(): Promise<Movie[]> {
+        const fileData:Buffer = await readFile('C:\\Users\\Mitchell\\Desktop\\Blockbuster\\movies.txt');
+        const textData:string = fileData.toString();
+        const movies:Movie[] = JSON.parse(textData);
+        return movies;        
     }
-    getMovieById(movieId: number): Promise<Movie> {
-        throw new Error("Method not implemented.");
+    async getMovieById(movieId: number): Promise<Movie> {
+        const fileData:Buffer = await readFile('C:\\Users\\Mitchell\\Desktop\\Blockbuster\\movies.txt');
+        const textData:string = fileData.toString();
+        const movies:Movie[] = JSON.parse(textData);
+        for(const movie of movies){
+            if(movie.movieId === movieId){
+                return  movie;
+            } 
+        }
     }
     updateMovie(movie: Movie): Promise<Movie> {
         throw new Error("Method not implemented.");
